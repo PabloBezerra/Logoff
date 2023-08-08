0f00d233b6b5fcb72ac2374ab20bd9a70d1a2d31
Pablo Bezerra <Pdsbezerra1316@gmail.com>
2023-08-08 01:03:48 -0300

    atualizando versão

--- version.2.2/PYQT.py
+++ version.2.2/PYQT.py
@@ -1,4 +1,6 @@
-from PyQt6 import uic,QtWidgets
+# IMPORTAÇÕES
+
+from PyQt6 import uic, QtWidgets
 import os
 from time import sleep
 
@@ -17,6 +19,7 @@ def cancelar():  # Comando de cancelamento de encerramento do windows
 def bloquear():  # Comando de bloquear o usuário do windows
     os.system('rundll32.exe user32.dll,LockWorkStation')
 
+
 # FUNÇÕES DE OPERAÇÃO
 
 def atualizar_pri(n): # Função interna de funcionamento da primeira dezena
@@ -31,10 +34,8 @@ def atualizar_pri(n): # Função interna de funcionamento da primeira dezena
     else:
         num = int(janela1.quadro_pri.text())
         num = num + 1 if n == 1 else num - 1
-        if num < 10:
-            janela1.quadro_pri.setText(f'0{num}')
-        else:
-            janela1.quadro_pri.setText(f'{num}')
+        janela1.quadro_pri.setText(f'{num:02d}')
+
 
 def atualizar_sec(n): # Função interna de funcionamento da segunda dezena
     if janela1.quadro_sec.text() == '01' and janela1.quadro_pri.text() == '00' and n == 0:
@@ -46,14 +47,12 @@ def atualizar_sec(n): # Função interna de funcionamento da segunda dezena
     elif janela1.quadro_sec.text() == '00' and n == 0:
         janela1.quadro_sec.setText('59')
     else:
-        num = (int(janela1.quadro_sec.text()))
+        num = int(janela1.quadro_sec.text())
         num = num + 1 if n == 1 else num - 1
-        if num < 10:
-            janela1.quadro_sec.setText(f'0{num}')
-        else:
-            janela1.quadro_sec.setText(f'{num}')
+        janela1.quadro_sec.setText(f'{num:02d}')
 
-def inicializador():
+
+def inicializador():  # função interna de tratamento e distribuição de dados
     if janela1.modo.currentText() == 'Desligar':
         variaveis[0] = 's'
         variaveis[1] = 'Desligado'
@@ -64,36 +63,48 @@ def inicializador():
     h = int(janela1.quadro_pri.text())
     m = int(janela1.quadro_sec.text())
 
-    variaveis[2] = 'Hora' if h == 1 or h == 0 else 'Horas'
-    variaveis[3] = 'Minuto' if m == 1 or m == 0 else 'Minutos'
-
-    #sair(h,m)
+    sair(h,m)
     sucesso(h, m)
-    contador(h, m)
 
-def sucesso(h, m):
+
+def sucesso(h, m):  # função interna da tela de sucesso e do timer de contagem regressiva
     janela1.close()
     janela2.show()
-    janela2.informacao1.setText(f'Seu Pc será {variaveis[1]}')
+    janela2.informacao_1.setText(f'Seu Pc será {variaveis[1]} em:')
+    s = 00
+    hs = h
+    mn = m
+    while True:
+        if s <= 0:
+            mn -= 1
+            s = 59
+        else:
+            s -= 1
+        if mn < 0:
+            hs -= 1
+            mn = 59
+        janela2.informacao_h.setText(f'{hs:02d} : {mn:02d} : {s:02d}')
+        QtWidgets.QApplication.processEvents()
+        sleep(1)  
 
-def contador(h,m):
-    
 
-def cancelamento():
+def cancelamento():  # função interna de cancelamento da operação
     janela2.close()
     janela3.show()
     cancelar()
     
-def ok():
+
+def ok():  # função interna de retorno a tela principal
     janela3.close()
     janela1.show()
 
-def finalizador():
+
+def finalizador():  # função interna de encerramento do programa e bloqueio do usuário caso requerido
     if janela2.check.isChecked():
         bloquear()
-
     janela2.close()
     exit()
+
 
 # VARIAVEIS
 
@@ -101,15 +112,18 @@ variaveis = ['s', 'Desligado']
 
 # INICIANDO O SITEMA
 
+# Inicializando o programa
 app = QtWidgets.QApplication([])
 janela1 = uic.loadUi('Principal.ui')
 janela2 = uic.loadUi('Sucesso.ui')
 janela3 = uic.loadUi('Cancelado.ui')
 
+# Configurando janelas
 janela1.show()
 janela2.close()
 janela3.close()
 
+# Configurando botões da primeira principal
 janela1.up_pri.clicked.connect(lambda: atualizar_pri(1))
 janela1.down_pri.clicked.connect(lambda: atualizar_pri(0))
 janela1.up_sec.clicked.connect(lambda: atualizar_sec(1))
@@ -117,9 +131,11 @@ janela1.down_sec.clicked.connect(lambda: atualizar_sec(0))
 
 janela1.btn_iniciar.clicked.connect(inicializador)
 
+# Configurando botões da primeira de sucesso
 janela2.btn_cancelar.clicked.connect(cancelamento)
 janela2.btn_finalizar.clicked.connect(finalizador)
 
+# Configurando botões da primeira de cancelamento
 janela3.btn_ok.clicked.connect(ok)
 
 app.exec()
